3126e7de1a67dce57e315db6d83315dad2fb6be6
sananabbasov <sanan_abbasov@list.ru>
2022-04-05 14:44:05 +0400

    05.04.2022

---
+++ ATM/Program.cs
@@ -0,0 +1,27 @@
+﻿string name = "Zulfu";
+int cardNumber = 1234567890;
+int password = 1234;
+int balans = 40;
+
+
+
+Console.Write("16 reqemli sifreni yazin: ");
+var currentCard = Console.ReadLine();
+
+Console.Write("Sifreni daxil edin: ");
+var currentPas = Console.ReadLine();
+
+
+if (cardNumber == Convert.ToInt32(currentCard) && password == Convert.ToInt32(currentPas))
+{
+    Console.WriteLine(name + "bey xos geldiniz");
+    Console.BackgroundColor = ConsoleColor.Green;
+    Console.WriteLine("Balansiniz: " + balans + " AZN");
+    Console.BackgroundColor = ConsoleColor.Black;
+}
+else
+{
+    Console.BackgroundColor = ConsoleColor.Red;
+    Console.WriteLine("16 reqemli kod ve ya sifre yanlisdi.");
+    Console.BackgroundColor = ConsoleColor.Black;
+}
